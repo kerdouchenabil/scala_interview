25fc4fd6626a667e5bb4bb2aa7d4f5439de85f04
kerdouchenabil <kerdouchenabil@gmail.com>
2020-06-07 08:34:51 +0200

    BasicScala answers

--- app/BasicScala.scala
+++ app/BasicScala.scala
@@ -17,13 +17,19 @@ object BasicScala {
     * input  : Map()
     * output : ""
     */
-  def encodeParamsInUrl(params: Map[String, String]): String = ???
+  def encodeParamsInUrl(params: Map[String, String]): String =
+  	if (params.isEmpty) "" else params.map{case (key, value) => s"$key=$value"}.mkString("?", "&", "")
 
 
   /**
     * Test if a String is an email
     */
-  def isEmail(maybeEmail: String): Boolean = ???
+  def isEmail(maybeEmail: String): Boolean = 
+  	if("""^(\w)@(\w)(\.)(\w)$""".r.findFirstIn(maybeEmail) == None)false else true
+  	/*
+  	\w for word so isEmail(a@b.c)=true
+  	println("result= ", isEmail("a@b.c"))
+  	*/
 
 
   /**
@@ -37,7 +43,8 @@ object BasicScala {
     * input : (i = 99, n = 38997)
     * output : 1723793299
     */
-  def power(i:Int, n:Int):Int = ???
-
+  def power(i:Int, n:Int):Int = 
+  	if (n==0) 1 else i*power(i, n-1)
+		/*Exception for i=99, n=38997*/
 
 }
